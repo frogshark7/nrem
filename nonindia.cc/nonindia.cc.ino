a8e793da24df7d8fe6e402fffe66309375022042
frogshark7 <irfan.nafi716@gmail.com>
2018-04-25 22:25:04 -0400

    Undo

--- nonindia.cc/nonindia.cc.ino
+++ nonindia.cc/nonindia.cc.ino
@@ -1,9 +1,5 @@
 #include <LiquidCrystal.h>
 
-float gjejiji;
-
-fjejieojoiwejo
-
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
