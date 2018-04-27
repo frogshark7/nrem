1f0b7de318f4cbd134eb71966a4c40d6c85bc4e3
frogshark7 <irfan.nafi716@gmail.com>
2018-04-27 00:21:33 -0400

    LCD

--- nonindia.cc/nonindia.cc.ino
+++ nonindia.cc/nonindia.cc.ino
@@ -1,29 +1,29 @@
 #include <LiquidCrystal.h>
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-float glucon;
+float glucon = 15;
 
 void setup() {
   // put your setup code here, to run once:
-  lcd.begin(16, 2);
-
+  lcd.begin(16, 1);
 }
 
 void loop() {
 
   if (glucon > 10) {
     lcd.display();
+    lcd.setCursor(4, 0);
     lcd.print("HIGH");
   } else if (glucon < 3.9) {
     lcd.display();
+    lcd.setCursor(3, 0);
     lcd.print("LOW");
   } else {
     lcd.display();
+    lcd.setCursor(5, 0);
     lcd.print("GOOD");
   }
-
 }
+
